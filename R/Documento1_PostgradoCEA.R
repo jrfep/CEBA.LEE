e4c1d6fcd2cc15c8f3f0a2b8ce6c9efbc65040f3
jrfep <jr.ferrer.paris@gmail.com>
2017-08-31 13:42:10 -0400

    actualizar documento

--- R/Documento1_PostgradoCEA.R
+++ R/Documento1_PostgradoCEA.R
@@ -72,16 +72,16 @@ str(lapply(html_nodes(pg,"table"),xml_text))
 ###################################################
 ### code chunk number 10: Combinar tablas de cada año
 ###################################################
-rm(rs)
+rm(lista.CEA)
 for (k in 1973:2017) {
     pg <- read_html(sprintf("EgresadosAño%s.html",k))
     rr <- html_table(html_nodes(pg,"table")[5],header=T,fill=T)[[1]]
     if (nrow(rr)>0) {
         rr$Año <- k
-        if (!exists("rs")) {
-            rs <- rr
+        if (!exists("lista.CEA")) {
+            lista.CEA <- rr
         } else {
-            rs <- rbind(rs[,colnames(rr)],rr)
+            lista.CEA <- rbind(lista.CEA[,colnames(rr)],rr)
         }
     }
 }
@@ -91,61 +91,61 @@ for (k in 1973:2017) {
 ###################################################
 ### code chunk number 11: Estructura del objeto
 ###################################################
-str(rs)
+str(lista.CEA)
 
 
 ###################################################
 ### code chunk number 12: Egresados por título otorgado
 ###################################################
-table(rs$"Grado Académico")
+table(lista.CEA$"Grado Académico")
 
 
 ###################################################
 ### code chunk number 13: Egresados por sexo
 ###################################################
-table(rs$Sexo,useNA="always")
+table(lista.CEA$Sexo,useNA="always")
 
 
 ###################################################
 ### code chunk number 14: Normalización de la columna
 ###################################################
-rs$Sexo <- tolower(rs$Sexo)
-table(rs$Sexo,useNA="always")
+lista.CEA$Sexo <- tolower(lista.CEA$Sexo)
+table(lista.CEA$Sexo,useNA="always")
 
 
 
 ###################################################
 ### code chunk number 15: Egresados por nacionalidad
 ###################################################
-table(rs$Nacionalidad,useNA="always")
+table(lista.CEA$Nacionalidad,useNA="always")
 
 
 ###################################################
 ### code chunk number 16: Egresados por áreas de estudio
 ###################################################
-table(rs$"Area de Estudio")
+table(lista.CEA$"Area de Estudio")
 
 
 ###################################################
 ### code chunk number 17: Distancia y clasificación jerárquica
 ###################################################
-d0 <- adist(rs$"Area de Estudio")
+d0 <- adist(lista.CEA$"Area de Estudio")
 h0 <- hclust(as.dist(d0))
 
 
 ###################################################
 ### code chunk number 18: Ejemplo de aplicación de dist. de Levenshtein
 ###################################################
-table(rs$"Area de Estudio",cutree(h0,h=3))[18:30,c(4:15,23)]
+table(lista.CEA$"Area de Estudio",cutree(h0,h=3))[18:30,c(4:15,23)]
 
 
 
 ###################################################
-### code chunk number 19: Versión corregida del área de estudio
+### code chunk number 19: Velista.CEAión corregida del área de estudio
 ###################################################
 c0 <- cutree(h0,h=3)
 for (j in unique(c0)) {
-    rs$area.corregida[c0 %in% j] <- names(  sort(table(rs[c0 %in% j,"Area de Estudio"]),decreasing=TRUE)[1])
+    lista.CEA$area.corregida[c0 %in% j] <- names(  sort(table(lista.CEA[c0 %in% j,"Area de Estudio"]),decreasing=TRUE)[1])
 
 }
 
@@ -153,22 +153,22 @@ for (j in unique(c0)) {
 ###################################################
 ### code chunk number 20: Verificación del resultado
 ###################################################
-table(rs$area.corregida)
+table(lista.CEA$area.corregida)
 
 
 
 ###################################################
 ### code chunk number 21: Corregir tres errores en la tabla
 ###################################################
-rs[grep("METALURGICA",rs$area.corregida),"area.corregida"] <- "METALURGICA Y CIENCIA DE LOS MATERIALES"
-rs[rs$"Area de Estudio" %in% "BIOLOGIA","area.corregida"] <- "BIOLOGIA"
-rs[rs$area.corregida %in% c("ESTUDIOS SOCIALES DE LA CIENCIA","ESTUDIOS DE LA CIENCIAS"),"area.corregida"] <- "ESTUDIOS SOCIALES DE LA CIENCIA"
+lista.CEA[grep("METALURGICA",lista.CEA$area.corregida),"area.corregida"] <- "METALURGICA Y CIENCIA DE LOS MATERIALES"
+lista.CEA[lista.CEA$"Area de Estudio" %in% "BIOLOGIA","area.corregida"] <- "BIOLOGIA"
+lista.CEA[lista.CEA$area.corregida %in% c("ESTUDIOS SOCIALES DE LA CIENCIA","ESTUDIOS DE LA CIENCIAS"),"area.corregida"] <- "ESTUDIOS SOCIALES DE LA CIENCIA"
 
 
 ###################################################
 ### code chunk number 22: EgresadosPorFecha
 ###################################################
-dts <- table(rs$Año,rs$area.corregida)
+dts <- table(lista.CEA$Año,lista.CEA$area.corregida)
 ss <- colSums(dts)>19
 
 svg(file=sprintf("%s/Egresados_IVIC_CEA.svg",output),width=8,height=8)
@@ -197,7 +197,7 @@ dev.off()
 require(plotrix)
 
 
-piramide <- with(rs,tapply(Estudiante,list(Año,tolower(Sexo)),length))
+piramide <- with(lista.CEA,tapply(Estudiante,list(Año,tolower(Sexo)),length))
 pyramid.plot(piramide[,"femenino"],piramide[,"masculino"],
              labels=rownames(piramide),gap=4,##xlim=c(-35,-1,1,35),
              lxcol="orangered",rxcol="slateblue",
@@ -209,6 +209,90 @@ pyramid.plot(piramide[,"femenino"],piramide[,"masculino"],
 ###################################################
 ### code chunk number 24: Documento1_PostgradoCEA.Rnw:310-311
 ###################################################
-table(rs$"Grado Académico",rs$Sexo)
+table(lista.CEA$"Grado Académico",lista.CEA$Sexo)
 
 
+###################################################
+### code chunk number 24: vamos a descargar ahora los datos del PEII
+###################################################
+
+
+PEII <- data.frame(id=c(120,115,110,108,100,109,102,111,117,118,99,113,116,98,103,106,121,101,104,105,97,112,119,107,114),
+                   estado=c("Yaracuy","Portuguesa","Lara","Falcon",
+                       "Apure","Guarico",
+                       "Barinas","Merida","Tachira","Trujillo",
+                       "Anzoategui","Monagas","Sucre",
+                       "Amazonas","Bolivar","Delta Amacuro",
+                       "Zulia",
+                       "Aragua","Carabobo","Cojedes",
+                       "Distrito Capital","Miranda","Vargas",
+                       "Dependencias Federales","Nueva Esparta"))
+
+for (k in 1:nrow(PEII)){
+    dst.file <- sprintf("PEII_%s.html",gsub(" ","_",PEII$estado[k]))
+    if (!file.exists(dst.file))
+        download.file(sprintf("http://www.oncti.gob.ve/index.php/component/content/article?id=%s",PEII$id[k]),dst.file)
+}
+
+pg <- read_html(sprintf("PEII_%s.html",gsub(" ","_",PEII$estado[k])))
+
+ html_nodes(pg,"table")
+   rr <- html_table(html_nodes(pg,"table")[2],header=T,fill=T)[[1]]
+
+rm(lista.PEII)
+for (k in 1:nrow(PEII)) {
+    pg <- read_html(sprintf("PEII_%s.html",gsub(" ","_",PEII$estado[k])))
+
+    for (j in 1:length(html_nodes(pg,"table"))) {
+        rr <- html_table(html_nodes(pg,"table")[j],header=T,fill=T)[[1]]
+        if (any(c("NOMBRE","NOMBRES") %in% colnames(rr))) {
+            if (nrow(rr)>0) {
+                rr$estado <- PEII$estado[k]
+                if (!exists("lista.PEII")) {
+                    lista.PEII <- rr
+                } else {
+                    colnames(rr) <- colnames(lista.PEII)
+                    lista.PEII <- rbind(lista.PEII,rr)
+                }
+            }
+        }
+    }
+}
+
+table(lista.PEII$estado)
+
+table(subset(lista.PEII,CEDULA %in% lista.CEA$Cédula)$estado)
+
+##¿306 o 280? existen varias personas con dos títulos (MSc y PhD)
+
+table(lista.CEA$Cédula %in% lista.PEII$CEDULA)
+require(RJSONIO)
+
+cr.cea <- list()
+autores.ttl <- data.frame()
+for (j in 1:nrow(lista.CEA)) {
+    print(j)
+    buscar <- lista.CEA$Estudiante[j]
+    
+    bb <- strsplit(buscar," ")[[1]]
+    
+    crossref.1 <- fromJSON(sprintf("http://api.crossref.org/works?query=%s&rows=1000",gsub(" ","+",buscar)))
+    
+    autores <- data.frame()
+    for (k in 1:1000) {
+        aas <- unlist(sapply(crossref.1$message$items[[k]]$author,function(x) unlist(iconv(paste(x$family,x$given),"latin1","utf8"))))
+        if (length(aas)>0) {
+            autores <- rbind(autores,
+                             data.frame(k,doi=crossref.1$message$items[[k]]$DOI,
+                                        autor=as.character(aas),
+                                        buscar=buscar))
+        }
+    }
+    autores$m[gsub("-"," ",tolower(autores$autor)) %in% tolower(buscar)] <- "exacto"
+    autores$m[gsub("-"," ",tolower(autores$autor)) %in% tolower(paste(bb[c(1,3)]))] <- "apellido nombre"
+    autores$m[gsub("\\.","",gsub("-"," ",tolower(autores$autor))) %in% tolower(paste(bb[1],bb[2],substr(bb[3],1,1)))] <- "apellidos inicial"
+    
+    autores.ttl <- rbind(autores.ttl,subset(autores,!is.na(m)))
+    
+    cr.cea[[j]] <- crossref.1$message$items[subset(autores,!is.na(m))$k]
+}
