2a5e34ac584d3db7a4c6a324666c8775c92ee46e
jrfep <jr.ferrer.paris@gmail.com>
2019-03-06 11:31:15 -0500

    datos publicaciones LRE

--- R/inc03_impactsRLEpubs.R
+++ R/inc03_impactsRLEpubs.R
@@ -9,6 +9,10 @@ require(rAltmetric)
 require(RJSONIO)
 require(rcrossref)
 require(xml2)
+##require(dplyr)
+require(tidyverse)
+require(gdata)
+
 
 mi.key <- ""
 
@@ -23,10 +27,167 @@ RLEpubs <- read.csv("~/mi.git/CEBA.LEE/data/IUCN_KP_Publications.csv")
 mis.dois <- unique(as.character(RLEpubs$DOI))
 mis.dois <- mis.dois[mis.dois!=""]
 
-
 slc.RLE <- mis.dois %in% subset(RLEpubs,RLE)$DOI
 slc.RLTS <- mis.dois %in% subset(RLEpubs,RLTS)$DOI
 slc.KBA <- mis.dois %in% subset(RLEpubs,KBA)$DOI
+
+ref.info <- cr_works(dois = mis.dois,.progress="text")
+
+## Intro en https://poldham.github.io/abs/crossref.html
+ref.info$data %>% count(subject, sort = TRUE)
+ref.info$data %>% separate_rows(subject, sep = ",") %>% 
+  count(subject=trim(subject), sort = TRUE) -> subjects  # output subjects
+
+library(ggplot2)
+subjects %>% 
+  ggplot2::ggplot(aes(subject, n, fill = subject)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  coord_flip()
+
+library(stringr)
+library(ggplot2)
+library(plotly)
+
+ref.info$data$year <- as.numeric(substr(ref.info$data$issued,0,4))
+ref.info$data %>% 
+    count(year) %>% 
+        ggplot(aes(x = year, y = n, group = 1)) +
+                geom_line() -> out # data out
+
+out
+
+
+library(tidyr)
+# Remove null, unnest list column, create full_name
+
+list.authors <- ref.info$data[ref.info$data$author != "NULL", ] %>% 
+  tidyr::unnest(., author) %>%
+  tidyr::unite(auth_full_name, c(family, given), sep = " ", remove = FALSE) %>% 
+  rename("auth_family" = family, "auth_given" = given, "auth_aff" = affiliation.name)
+
+# get initials...
+list.authors$initials <- tolower(sapply(list.authors$auth_given,function(x) {
+    y <- strsplit(x," ")[[1]]
+    paste(substr(y,1,1),collapse="")
+}))
+
+## last name + initials
+list.authors$valid_name <- trim(tolower(paste(list.authors$auth_family,list.authors$initials,sep=" ")))
+
+
+
+library(dplyr)
+library(stringr)
+
+length(unique(list.authors$valid_name))
+length(unique(list.authors$auth_full_name))
+
+
+list.authors %>% count(valid_name, sort = TRUE)
+
+list.authors$auth_alt2 <- list.authors$valid_name
+list.authors$auth_alt3 <- list.authors$valid_name
+list.authors$auth_alt4 <- list.authors$valid_name
+mtz.names <- adist(list.authors$valid_name)
+
+for (k in unique(list.authors$valid_name)) {
+    idx <- first(match(k,list.authors$auth_alt2))
+    if (length(idx)==1) {
+        list.authors$auth_alt2[which(mtz.names[idx,]<2)] <- k
+        list.authors$auth_alt3[which(mtz.names[idx,]<3)] <- k
+        list.authors$auth_alt4[which(mtz.names[idx,]<4)] <- k
+    }
+}
+
+list.authors %>% count(auth_alt2, sort = TRUE) ## poco
+list.authors %>% count(auth_alt3, sort = TRUE) ## mejor
+subset(list.authors,auth_alt3 %in% "butchart s")$valid_name
+
+list.authors %>% count(auth_alt4, sort = TRUE) ## demasiado
+
+length(unique(list.authors$auth_alt3))
+length(unique(list.authors$valid_name))
+
+
+mtz <- as.matrix(with(subset(list.authors,doi %in% subset(RLEpubs,RLE)$DOI),table(doi,auth_alt3)))
+co_occurrence1 <- t(mtz) %*% mtz
+
+mtz2 <- as.matrix(with(subset(list.authors,doi %in% subset(RLEpubs,RLTS | KBA)$DOI),table(doi,auth_alt3)))
+co_occurrence2 <- t(mtz2) %*% mtz2
+##mtz <- mtz[,colSums(mtz)>1]
+##mtz <- mtz[rowSums(mtz)>1,]
+##mtz <- mtz[,colSums(mtz)>1]
+
+##http://kateto.net/netscix2016
+require(igraph)
+
+layout(matrix(c(1,1,3,3,
+                1,1,3,3,
+                2,2,4,4),ncol=4,byrow=T))
+par(mar=c(0,0,1,0))
+
+g1 <- graph.adjacency(co_occurrence1,
+                         weighted=TRUE,
+                         mode="undirected",
+                         diag=FALSE)
+deg <- degree(g1, mode="all")
+
+
+md <- rev(sort(degree(g1)))[1:10]
+mc <- rev(sort(closeness(g1)))[1:10]
+mb <- rev(sort(betweenness(g1)))[1:10]
+
+l1 <- layout_with_kk(g1)
+n <- V(g1)$name
+
+plot(g1, layout=l1,edge.arrow.size=.15, vertex.color="gold", vertex.size=sqrt(deg)/2,vertex.label=NA) ## vertex.label=ifelse(n %in% c(names(md),names(mc),names(mb)),n,NA)
+
+par(mar=c(4,4,0,0))
+hist(deg, breaks=seq(0,vcount(g2),length=20), main="Histogram of node degree")
+
+par(mar=c(0,0,1,0))
+
+g2 <- graph.adjacency(co_occurrence2,
+                         weighted=TRUE,
+                         mode="undirected",
+                         diag=FALSE)
+deg <- degree(g2, mode="all")
+
+md <- rev(sort(degree(g2)))[1:10]
+mc <- rev(sort(closeness(g2)))[1:10]
+mb <- rev(sort(betweenness(g2)))[1:10]
+
+
+l2 <- layout_with_kk(g2)
+n <- V(g2)$name
+
+plot(g2, layout=l2,edge.arrow.size=.15, vertex.color="gold", vertex.size=sqrt(deg)/2,vertex.label=NA)
+     ##vertex.label=ifelse(n %in% c(names(md),names(mc),names(mb)),n,NA))
+
+par(mar=c(4,4,0,0))
+hist(deg, breaks=seq(0,vcount(g2),length=20), main="Histogram of node degree")
+
+
+
+
+################
+##
+
+l <- layout_in_circle(g)
+l <- layout_on_sphere(g)
+               l <- layout_with_fr(g)
+l <- layout_with_kk(g)
+
+coords <- layout_(g, nicely())
+
+plot(g, edge.arrow.size=.5, vertex.color="gold", vertex.size=15, 
+     vertex.frame.color="gray", vertex.label.color="black", 
+     vertex.label.cex=0.8, vertex.label.dist=2, edge.curved=0.2) 
+
+plot(g, layout=coords,
+main='Coauthor networks', vertex.label.dist=0.15, vertex.frame.color='maroon', vertex.label.color='black', vertex.label.font=2, vertex.label=V(g)$name, vertex.label.cex=.53)
+
+ 
 
 table(slc.RLTS | slc.KBA, slc.RLE)
 
